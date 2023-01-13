7930762807a9f8ee11231cf15746405ffedf68fa
Shiska07 <rautshiska@gmail.com>
2023-01-12 19:22:10 -0600

    test merge sort

--- Sorting Algorithms/sorting_algorithms.py
+++ Sorting Algorithms/sorting_algorithms.py
@@ -13,15 +13,43 @@ def insertion_sort(arr):
         arr[i + 1] = key
     print(arr)
 
+
 def merge(arr, p, q, r):
-    n1 = 
+    n1 = q - p + 1
+    n2 = r - q
+
+    # left and right subarrays
+    L = np.zeros(n1, np.int32)
+    R = np.zeros(n2, np.int32)
+
+    for i in range(0, n1):
+        L[i] = arr[p + i]
+
+    for i in range(0, n2):
+        R[i] = arr[q + 1 + i]
+
+    i = 0
+    j = 0
+    k = p
+
+    while (k >= p) and (k <= r):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
 
 def merge_sort(arr, p, r):
     if p < r:
-        q = math.floor((p + r)/2)
+        q = p + (r-1)//2
         merge_sort(arr, p, q)
         merge_sort(arr, q+1, r)
         merge(arr, p, q, r)
+    print(arr)
+
 
 def quick_sort(arr):
     pass
