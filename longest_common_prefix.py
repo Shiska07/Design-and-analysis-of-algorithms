83e961341e0cd10592d367e1a7009b8e371eb668
Shiska07 <rautshiska@gmail.com>
2023-02-07 23:59:19 -0600

    optimize

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,20 +1,25 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        longest = []
+        n = len(strs)
+        i = 0
+        match = 1
+        count = 0
+        len_smallest = len(strs[0])
         for item in strs:
-            k = 0
-            i = 1
-            count = 0
-            pre = []  # prefix list
-            pre.append(0)
-            while i < len(item):
-                if item[i] == item[k]:
-                    pre.append(pre[i - 1] + 1)
-                    i += 1
-                    k += 1
-                else:
-                    if pre[i - 1] > count:
-                        count = pre[i - 1]
-                    k = pre[k - 1]
-            longest.append(count)
-        min = min(longest)
+            if len(item) < len_smallest:
+                len_smallest = len(item)
+        while i < len_smallest:
+            j = 1
+            while j < n:
+                match = (strs[0][i] == strs[j][i])
+                if match:
+                    j += 1
+            if j == n:
+                count += 1
+                i += 1
+            else:
+                break
+        if count > 0:
+            return strs[0][0:count]
+        else:
+            return ""
